8cc2e1dcaa9592abe8f8f86be6c8ad2f2a3da608
Sciola <115746943+sciola-framework@users.noreply.github.com>
2022-11-08 15:58:50 -0300

    Initial commit

---
+++ classes/MyClass.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * MyClass
+ *
+ * @version 1.0.0
+ */
+namespace Sciola;
+
+class MyClass
+{
+    /**
+     * __construct
+     *
+     * @access public
+     */
+    public function __construct()
+    {
+
+    }
+}
